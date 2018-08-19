9fb61b92963c96634dcd156dd4093323596e933d
homusuika <homugiken@gmail.com>
2018-08-19 22:05:50 +0800

    On branch DebugServer
    finishing dbgmsg & dbgmsg_svr.
    
    Changes to be committed:
    	modified:   DebugServer/src/DebugServer.c
    	modified:   DebugServer/src/DebugServer.h

--- DebugServer/src/DebugServer.c
+++ DebugServer/src/DebugServer.c
@@ -9,10 +9,12 @@
 /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*/
 typedef struct GLOBAL_CFG {
     DBG_SVR_CFG                         _dbg_svr, * dbg_svr;
+    DBGMSG_CFG                          _dbgmsg, * dbgmsg;
 } GLOBAL_CFG;
 
 typedef struct GLOBAL_CTL {
     DBG_SVR_CTL                         _dbg_svr, * dbg_svr;
+    DBGMSG_CTL                          _dbgmsg, * dbgmsg;
 } GLOBAL_CTL;
 
 static GLOBAL_CFG                       _gcfg, * const gcfg = &_gcfg;
@@ -34,10 +36,10 @@ static struct option                    optlist[] =
     {DBG_SVR_OPTL_LOG_SIZE,             required_argument,  0,                  DBG_SVR_OPTC_LOG_SIZE},
     {DBG_SVR_OPTL_LOG_COUNT,            required_argument,  0,                  DBG_SVR_OPTC_LOG_COUNT},
     {DBG_SVR_OPTL_DBGMSG_SVR,           no_argument,        0,                  DBG_SVR_OPTC_DBGMSG_SVR},
-    /* DBGMSG_SVR */
-    {DBGMSG_SVR_OPTL_KEY,               required_argument,  0,                  DBGMSG_SVR_OPTC_KEY},
-    {DBGMSG_SVR_OPTL_KEY_PATH,          required_argument,  0,                  0},
-    {DBGMSG_SVR_OPTL_KEY_ID,            required_argument,  0,                  0},
+    /* DBGMSG */
+    {DBGMSG_OPTL_KEY,                   required_argument,  0,                  DBGMSG_OPTC_KEY},
+    {DBGMSG_OPTL_KEY_PATH,              required_argument,  0,                  0},
+    {DBGMSG_OPTL_KEY_ID,                required_argument,  0,                  0},
     {0, 0, 0, 0}
 };
 
@@ -54,7 +56,6 @@ dbgstd_printf (
     ...)
 {
     static DBGSTD_CTL                   _ctl, * const ctl = &_ctl;
-
 
     ctl->time_str[0] = '\0';
     ctl->time_now = time(NULL);
@@ -78,44 +79,185 @@ dbgstd_printf (
 /*------------------------------------*/
 static void
 dbgmsg_printf (
-    DBGMSG_CTL * const                  ctl,
     const char *                        fmt,
     ...)
 {
-    int                                 ret = -1;
-    DBGMSG_MSG                          _msg, * const msg = &_msg;
+    DBGMSG_CTL * const                  ctl = gctl->dbgmsg;
+    static DBGMSG_MSG                   _msg, * const msg = &_msg;
 
-    if ((ctl == NULL) || (ctl->qid < 0) || (ctl->ready != true))
-    {
-        return;
-    }
+    if (ctl == NULL) { return; }
+    if (ctl->ready != true) { return; }
+    if (ctl->qid < 0) { return; }
+    MEMZ(msg, sizeof(DBGMSG_MSG));
 
     msg->type = DBGMSG_TYPE_DEBUG;
     msg->src_time = time(NULL);
     msg->src_pid = ctl->pid;
+    if (ctl->cfg != NULL)
+    {
+        snprintf(msg->src_name, DBGMSG_SRC_NAME_LEN, "%s", ctl->cfg->src_name);
+    }
 
-    msg->src_name[0] = '\0';
-    snprintf(msg->src_name, DBGMSG_SRC_NAME_LEN, "%s", ctl->src_name);
-
-    msg->text[0] = '\0';
     va_start(ctl->vargs, fmt);
     vsnprintf(msg->text, DBGSTD_TEXT_LEN, fmt, ctl->vargs);
     va_end(ctl->vargs);
 
-    msgsnd(ctl->qid, msg, (sizeof(time_t) + sizeof(pid_t) + DBGMSG_SRC_NAME_LEN + strlen(msg->text) + 1), IPC_NOWAIT);
+    msgsnd(ctl->qid, msg, (sizeof(time_t) + sizeof(pid_t) + DBGMSG_SRC_NAME_LEN + strlen(msg->text)), IPC_NOWAIT);
 }
 
+static void
+dbgmsg_cfg_show (
+    DBGMSG_CFG * const                  cfg)
+{   ENTR();
+    ERR_NULL(cfg);
 
-static void dbgmsg_cfg_show (DBGMSG_CFG * const cfg);
-static int dbgmsg_cfg (DBGMSG_CFG * const cfg, const int argc, char * const argv[]);
-static void dbgmsg_help (void);
-static void dbgmsg_release (DBGMSG_CTL * const ctl);
-static int dbgmsg_init (DBGMSG_CTL * const ctl, DBGMSG_CFG * const cfg);
+    LOG("key=0x%08X", cfg->key);
+    LOG("key_path=\"%s\"", cfg->key_path);
+    LOG("key_id=0x%02X", cfg->key_id);
 
+LEXIT;
+    return;
+LERROR;
+    GOEXIT;
+}
 
+static int
+dbgmsg_cfg (
+    DBGMSG_CFG * const                  cfg,
+    const int                           argc,
+    char * const                        argv[])
+{   ENTR();
+    int                                 ret = -1;
+    int                                 optchar;
+    int                                 optindex;
+    ERR_NULL(cfg); ERR_NPOS(argc); ERR_NULL(argv);
 
+    MEMZ(cfg, sizeof(DBGMSG_CFG));
+    optind = 0;
+    while (1)
+    {
+        optchar = getopt_long_only(argc, argv, "", optlist, &optindex);
+        if (optchar == -1) { break; }
 
+        switch (optchar)
+        {
+        case DBGMSG_OPTC_KEY:
+            ERR_OPTARG_INVALID();
+            cfg->key = strtoul(optarg, NULL, 0);
+            INF("key=0x%08X", cfg->key);
+            break;
+        case 0:
+            if (strncmp(optlist[optindex].name, DBGMSG_OPTL_KEY_PATH, strlen(DBGMSG_OPTL_KEY_PATH)) == 0)
+            {
+                ERR_OPTARG_INVALID();
+                snprintf(cfg->key_path, DBGMSG_KEY_PATH_LEN, "%s", optarg);
+                INF("key_path=\"%s\"", cfg->key_path);
+            }
+            else if (strncmp(optlist[optindex].name, DBGMSG_OPTL_KEY_ID, strlen(DBGMSG_OPTL_KEY_ID)) == 0)
+            {
+                ERR_OPTARG_INVALID();
+                cfg->key_id = strtoul(optarg, NULL, 0);
+                INF("key_id=0x%02X", cfg->key_id);
+            }
+            break;
+        default:
+                break;
+        }
+    }
 
+    if (strlen(cfg->key_path) < 2)
+    {
+        snprintf(cfg->key_path, DBGMSG_KEY_PATH_LEN, "%s", DBGMSG_KEY_PATH_DFT);
+        INF("key_path=\"%s\"", cfg->key_path);
+    }
+
+    if ((cfg->key_id < DBGMSG_KEY_ID_MIN) || (cfg->key_id > DBGMSG_KEY_ID_MAX))
+    {
+        cfg->key_id = DBGMSG_KEY_ID_DFT;
+        INF("key_id=0x%02X", cfg->key_id);
+    }
+    ERR_RANGE(cfg->key_id, DBGMSG_KEY_ID_MIN, DBGMSG_KEY_ID_MAX);
+
+    if (cfg->key < 1)
+    {
+        cfg->key = ftok(cfg->key_path, cfg->key_id); ERR_NEG(cfg->key);
+        INF("key=0x%X", cfg->key);
+    }
+
+    ret = 0;
+LEXIT;
+    return(ret);
+LERROR;
+    GOEXIT;
+}
+
+static void
+dbgmsg_help (void)
+{   ENTR();
+
+    printf("-%c/--%s\t%s\r\n", DBGMSG_OPTC_KEY, DBGMSG_OPTL_KEY, DBGMSG_OPTS_KEY);
+    printf("--%s\t%s\r\n", DBGMSG_OPTL_KEY_PATH, DBGMSG_OPTS_KEY_PATH);
+    printf("--%s\t\t%s\r\n", DBGMSG_OPTL_KEY_ID, DBGMSG_OPTS_KEY_ID);
+
+LEXIT;
+    return;
+}
+
+static void
+dbgmsg_release (
+    DBGMSG_CTL * const                  ctl)
+{   ENTR();
+    int                                 ret = -1;
+    ERR_NULL(ctl);
+
+    if (ctl->qid >=0)
+    {
+        ret = msgctl(ctl->qid, IPC_RMID, NULL); WRN_NZERO(ret);
+        ctl->qid = -1;
+        ctl->ready = false;
+    }
+
+    MEMZ(ctl, sizeof(DBGMSG_CTL));
+
+LEXIT;
+    return;
+LERROR;
+    GOEXIT;
+}
+
+static int
+dbgmsg_init (
+    DBGMSG_CTL * const                  ctl,
+    DBGMSG_CFG * const                  cfg)
+{   ENTR();
+    int                                 ret = -1;
+    ERR_NULL(ctl); ERR_NULL(cfg);
+
+    if (ctl->cfg != NULL)
+    {
+        dbgmsg_release(ctl);
+    }
+    ctl->cfg = cfg;
+
+    ERR_NPOS(ctl->cfg->key);
+    ctl->qid = msgget(ctl->cfg->key, (IPC_CREAT | IPC_EXCL));
+    if ((ctl->qid < 0) && (errno == EEXIST))
+    {
+        ctl->qid = msgget(ctl->cfg->key, 0); WRN_NEG(ctl->qid);
+    }
+    ERR_NEG(ctl->qid);
+    LOG("qid=%d", ctl->qid);
+
+    ctl->ready = true;
+    LOG("ready=%s", STRBOOL(ctl->ready));
+
+    ret = 0;
+LEXIT;
+    return(ret);
+LERROR;
+    dbgmsg_release(ctl);
+    GOEXIT;
+}
 
 /*____________________________________________________________________________*/
 /* DBGMSG_SVR */
@@ -126,8 +268,7 @@ dbgmsg_svr_fprintf (
     FILE * const                        fp)
 {   ENTR();
     int                                 ret = -1;
-    DBGMSG_MSG *                    msg = NULL;
-
+    DBGMSG_MSG *                        msg = NULL;
     ERR_NULL(ctl); ERR_NULL(fp);
     ERR_FALSE(ctl->ready);
 
@@ -143,12 +284,12 @@ dbgmsg_svr_fprintf (
         ctl->time_msg = msg->src_time;
         ctl->time_local = localtime(&(ctl->time_msg));
 
-        ret = fprintf(fp, DBGMSG_SVR_MSG_FMT,
+        ret = fprintf(fp, DBGMSG_MSG_FMT,
                       ctl->LOCAL_YEAR, ctl->LOCAL_MON, ctl->LOCAL_DAY,
                       ctl->LOCAL_HOUR, ctl->LOCAL_MIN, ctl->LOCAL_SEC,
                       msg->src_pid, msg->src_name, msg->text); WRN_NPOS(ret);
 
-        ret = fprintf(stdout, DBGMSG_SVR_MSG_FMT,
+        ret = fprintf(stdout, DBGMSG_MSG_FMT,
                       ctl->LOCAL_YEAR, ctl->LOCAL_MON, ctl->LOCAL_DAY,
                       ctl->LOCAL_HOUR, ctl->LOCAL_MIN, ctl->LOCAL_SEC,
                       msg->src_pid, msg->src_name, msg->text); WRN_NPOS(ret);
@@ -166,23 +307,21 @@ dbgmsg_svr_rcv (
     DBGMSG_SVR_CTL * const              ctl)
 {   ENTR();
     int                                 ret = -1;
-
     ERR_NULL(ctl);
-    ERR_FALSE(ctl->ready);
-    ERR_NEG(ctl->qid);
+    ERR_FALSE(ctl->ready); ERR_NULL(ctl->dbgmsg); ERR_NEG(ctl->dbgmsg->qid);
 
     if (ctl->msg_count > 0)
     {
-        MEMZ(ctl->msg_buf, (ctl->msg_count * sizeof(DBGMSG_SVR_MSG)));
+        MEMZ(ctl->msg_buf, (ctl->msg_count * sizeof(DBGMSG_MSG)));
     }
     ctl->msg_count = 0;
 
     for (int i = 0; i < DBGMSG_SVR_MSG_BUF_SIZE; i++)
     {
-        ret = msgrcv(ctl->qid,
+        ret = msgrcv(ctl->dbgmsg->qid,
                      &(ctl->msg_buf[i]),
-                     (sizeof(DBGMSG_SVR_MSG) - sizeof(long)),
-                     0,
+                     (sizeof(DBGMSG_MSG) - sizeof(long)),
+                     DBGMSG_TYPE_DEBUG,
                      (IPC_NOWAIT | MSG_NOERROR)); WRN_NPOS(ret);
         if (ret <= 0)
         {
@@ -206,12 +345,12 @@ static void
 dbgmsg_svr_cfg_show (
     DBGMSG_SVR_CFG * const              cfg)
 {   ENTR();
-
     ERR_NULL(cfg);
 
-    LOG("key=0x%08X", cfg->key);
-    LOG("key_path=\"%s\"", cfg->key_path);
-    LOG("key_id=0x%02X", cfg->key_id);
+    if (cfg->dbgmsg != NULL)
+    {
+        dbgmsg_cfg_show(cfg->dbgmsg);
+    }
 
 LEXIT;
     return;
@@ -225,64 +364,11 @@ dbgmsg_svr_cfg (
     const int                           argc,
     char * const                        argv[])
 {   ENTR();
-
     int                                 ret = -1;
-    int                                 optchar;
-    int                                 optindex;
-
     ERR_NULL(cfg); ERR_NPOS(argc); ERR_NULL(argv);
 
-    MEMZ(cfg, sizeof(DBGMSG_SVR_CFG));
-    optind = 0;
-    while (1)
-    {
-        optchar = getopt_long_only(argc, argv, "", optlist, &optindex);
-        if (optchar == -1) { break; }
-
-        switch (optchar)
-        {
-        case DBGMSG_SVR_OPTC_KEY:
-            ERR_OPTARG_INVALID();
-            cfg->key = strtoul(optarg, NULL, 0);
-            INF("key=0x%08X", cfg->key);
-            break;
-        case 0:
-            if (strncmp(optlist[optindex].name, DBGMSG_SVR_OPTL_KEY_PATH, strlen(DBGMSG_SVR_OPTL_KEY_PATH)) == 0)
-            {
-                ERR_OPTARG_INVALID();
-                snprintf(cfg->key_path, DBGMSG_SVR_KEY_PATH_LEN, "%s", optarg);
-                INF("key_path=\"%s\"", cfg->key_path);
-            }
-            else if (strncmp(optlist[optindex].name, DBGMSG_SVR_OPTL_KEY_ID, strlen(DBGMSG_SVR_OPTL_KEY_ID)) == 0)
-            {
-                ERR_OPTARG_INVALID();
-                cfg->key_id = strtoul(optarg, NULL, 0);
-                INF("key_id=0x%02X", cfg->key_id);
-            }
-            break;
-        default:
-                break;
-        }
-    }
-
-    if (strlen(cfg->key_path) < 2)
-    {
-        snprintf(cfg->key_path, DBGMSG_SVR_KEY_PATH_LEN, "%s", DBGMSG_SVR_KEY_PATH_DFT);
-        INF("key_path=\"%s\"", cfg->key_path);
-    }
-
-    if ((cfg->key_id < DBGMSG_SVR_KEY_ID_MIN) || (cfg->key_id > DBGMSG_SVR_KEY_ID_MAX))
-    {
-        cfg->key_id = DBGMSG_SVR_KEY_ID_DFT;
-        INF("key_id=0x%02X", cfg->key_id);
-    }
-    ERR_RANGE(cfg->key_id, DBGMSG_SVR_KEY_ID_MIN, DBGMSG_SVR_KEY_ID_MAX);
-
-    if (cfg->key < 1)
-    {
-        cfg->key = ftok(cfg->key_path, cfg->key_id); ERR_NEG(cfg->key);
-        INF("key=0x%X", cfg->key);
-    }
+    cfg->dbgmsg = gcfg->dbgmsg; ERR_NULL(cfg->dbgmsg);
+    ret = dbgmsg_cfg(cfg->dbgmsg, argc, argv); ERR_NZERO(ret);
 
     ret = 0;
 LEXIT;
@@ -295,9 +381,7 @@ static void
 dbgmsg_svr_help (void)
 {   ENTR();
 
-    printf("-%c/--%s\t%s\r\n", DBGMSG_SVR_OPTC_KEY, DBGMSG_SVR_OPTL_KEY, DBGMSG_SVR_OPTS_KEY);
-    printf("--%s\t%s\r\n", DBGMSG_SVR_OPTL_KEY_PATH, DBGMSG_SVR_OPTS_KEY_PATH);
-    printf("--%s\t\t%s\r\n", DBGMSG_SVR_OPTL_KEY_ID, DBGMSG_SVR_OPTS_KEY_ID);
+    dbgmsg_help();
 
 LEXIT;
     return;
@@ -307,15 +391,14 @@ static void
 dbgmsg_svr_release (
     DBGMSG_SVR_CTL * const              ctl)
 {   ENTR();
-    int                                 ret = -1;
-
     ERR_NULL(ctl);
 
-    if (ctl->qid >=0)
+    if (ctl->dbgmsg != NULL)
     {
-        ret = msgctl(ctl->qid, IPC_RMID, NULL); WRN_NZERO(ret);
-        ctl->ready = false;
+        dbgmsg_release(ctl->dbgmsg);
     }
+
+    MEMZ(ctl, sizeof(DBGMSG_SVR_CTL));
 
 LEXIT;
     return;
@@ -329,7 +412,6 @@ dbgmsg_svr_init (
     DBGMSG_SVR_CFG * const              cfg)
 {   ENTR();
     int                                 ret = -1;
-
     ERR_NULL(ctl); ERR_NULL(cfg);
 
     if (ctl->cfg != NULL)
@@ -338,16 +420,10 @@ dbgmsg_svr_init (
     }
     ctl->cfg = cfg;
 
-    ERR_NPOS(ctl->cfg->key);
-    ctl->qid = msgget(ctl->cfg->key, (IPC_CREAT | IPC_EXCL));
-    if ((ctl->qid < 0) && (errno == EEXIST))
-    {
-        ctl->qid = msgget(ctl->cfg->key, 0); WRN_NEG(ctl->qid);
-    }
-    ERR_NEG(ctl->qid);
-    LOG("qid=%d", ctl->qid);
+    ctl->dbgmsg = gctl->dbgmsg; ERR_NULL(ctl->dbgmsg);
+    ret = dbgmsg_init(ctl->dbgmsg, cfg->dbgmsg); ERR_NZERO(ret);
 
-    ctl->ready = true;
+    ctl->ready = ctl->dbgmsg->ready;
     LOG("ready=%s", STRBOOL(ctl->ready));
 
     ret = 0;
@@ -1166,7 +1242,9 @@ main_cfg (
     }
     ERR_RANGE(gverbose, DBG_SVR_VERBOSE_MIN, DBG_SVR_VERBOSE_MAX);
 
-    gcfg->dbg_svr = &(gcfg->_dbg_svr);
+    gcfg->dbg_svr = &(gcfg->_dbg_svr); ERR_NULL(gcfg->dbg_svr);
+    gcfg->dbgmsg = &(gcfg->_dbgmsg); ERR_NULL(gcfg->dbgmsg);
+
     ret = dbg_svr_cfg(gcfg->dbg_svr, argc, argv); ERR_NZERO(ret);
 
     ret = 0;
@@ -1218,7 +1296,9 @@ main_init (void)
 {   ENTR();
     int                                 ret = -1;
 
-    gctl->dbg_svr = &(gctl->_dbg_svr);
+    gctl->dbg_svr = &(gctl->_dbg_svr); ERR_NULL(gctl->dbg_svr);
+    gctl->dbgmsg = &(gctl->_dbgmsg); ERR_NULL(gctl->dbgmsg);
+
     ret = dbg_svr_init(gctl->dbg_svr, gcfg->dbg_svr); ERR_NZERO(ret);
 
     ret = 0;
--- DebugServer/src/DebugServer.h
+++ DebugServer/src/DebugServer.h
@@ -71,7 +71,7 @@
 #endif
 /*························································*/
 #if DBG_ENABLE_DBGMSG && DBG_ENABLE_ALL
-#define DBGMSG(fmt,...)                 dbgmsg_printf(gctl->dbgmsg_clnt->dbgmsg,"#%04d|%s:"fmt,__LINE__,__FUNCTION__,##__VA_ARGS__)
+#define DBGMSG(fmt,...)                 dbgmsg_printf("#%04d|%s:"fmt,__LINE__,__FUNCTION__,##__VA_ARGS__)
 #else
 #define DBGMSG(fmt,...)
 #endif
@@ -167,11 +167,11 @@ static void dbgstd_printf (const char * fmt, ...);
 /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*/
 #define DBGMSG_OPTL_KEY                 "key"
 #define DBGMSG_OPTC_KEY                 'k'
-#define DBGMSG_OPTS_KEY                 "dbgmsg msgkey"
+#define DBGMSG_OPTS_KEY                 "msgkey"
 #define DBGMSG_OPTL_KEY_PATH            "keypath"
-#define DBGMSG_OPTS_KEY_PATH            "dbgmsg msgkey path (used to generate msgkey when the msgkey was not assigned)"
+#define DBGMSG_OPTS_KEY_PATH            "msgkey path"
 #define DBGMSG_OPTL_KEY_ID              "keyid"
-#define DBGMSG_OPTS_KEY_ID              "dbgmsg msgkey id (used to generate msgkey when the msgkey was not assigned)"
+#define DBGMSG_OPTS_KEY_ID              "msgkey id"
 /*························································*/
 #define DBGMSG_TYPE_DEBUG               0x01
 /*························································*/
@@ -220,17 +220,16 @@ static int dbgmsg_init (DBGMSG_CTL * const ctl, DBGMSG_CFG * const cfg);
 /*____________________________________________________________________________*/
 /* DBGMSG_SVR */
 /*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯*/
-/*························································*/
 #define DBGMSG_SVR_MSG_BUF_SIZE         10
 #define DBGMSG_SVR_MSG_COUNT_MAX        (DBGMSG_SVR_MSG_BUF_SIZE - 1)
 /*························································*/
 typedef struct DBGMSG_SVR_CFG {
-    DBGMSG_CFG                          _dbgmsg, * dbgmsg;
+    DBGMSG_CFG *                        dbgmsg;
 } DBGMSG_SVR_CFG;
 /*························································*/
 typedef struct DBGMSG_SVR_CTL {
     DBGMSG_SVR_CFG *                    cfg;
-    int                                 qid;
+    DBGMSG_CTL *                        dbgmsg;
     bool                                ready;
     DBGMSG_MSG                          msg_buf[DBGMSG_SVR_MSG_BUF_SIZE];
     uint32_t                            msg_count;
@@ -344,54 +343,6 @@ static int dbg_svr_cfg (DBG_SVR_CFG * const cfg, const int argc, char * const ar
 static void dbg_svr_help (void);
 static void dbg_svr_release (DBG_SVR_CTL * const ctl);
 static int dbg_svr_init (DBG_SVR_CTL * const ctl, DBG_SVR_CFG * const cfg);
-
-/*____________________________________________________________________________*/
-/* DBGMSG_CLNT */
-/*------------------------------------*/
-#define DBGMSG_SRC_NAME_LEN             32
-
-
-
-
-/*························································*/
-typedef struct DBGMSG_CLNT_CFG {
-    key_t                               key;
-    char                                key_path[DBGMSG_SVR_KEY_PATH_LEN];
-    uint32_t                            key_id;
-    char                                src_name[DBGMSG_SRC_NAME_LEN]
-} DBGMSG_CLNT_CFG;
-/*························································*/
-typedef struct DBGMSG_CLNT_CTL {
-    DBGMSG_CLNT_CFG *                   cfg;
-    pid_t                               pid;
-    bool                                ready;
-    DBGMSG_MSG                          _msg, * msg;
-    time_t                              time_now;
-} DBGMSG_CLNT_CTL;
-
-/*____________________________________________________________________________*/
-/* DBG_CLNT */
-/*------------------------------------*/
-
-
-
-
-
-typedef struct DBG_CLNT_CFG {
-
-} DBG_CLNT_CFG;
-
-typedef struct DBG_CLNT_CTL {
-    DBG_CLNT_CFG *                      cfg;
-    pid_t                               pid;
-    pid_t                               ppid;
-    bool                                ready;
-
-
-
-} DBG_CLNT_CTL;
-
-
 
 /*____________________________________________________________________________*/
 /* MAIN */
